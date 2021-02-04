9058abfaee4a5de2b0f831d0f578d2dac851ea49
Steve Cline <steve.cline@weldre4.org>
2021-02-04 08:50:51 -0700

    Update Starter_while.ino

--- Starter_while.ino
+++ Starter_while.ino
@@ -1,11 +1,27 @@
+/* This sketch will be used to create a cycle where three lights turn
+on in sequence and the Serial monitor displays the status of those 
+lights. */
+
+// Initialize variables
+
 int x = 1;
 
 void setup()
 {
+    // Start Serial Moniter and set up pins
     Serial.begin(9600);
-    while (x < 5) {
+    pinMode ();
+    pinMode ();
+    pinMode ();
+    
+    while (x < ) {
+        // Display the cycle number in the Serial Monitor
         Serial.print("Cycle no ");
         Serial.println(x);
+        
+        // Turn on each LED in sequence
+        digitalWrite ((x + 7), );
+        delay (2000)
         x = x + 1;
     }
 }
